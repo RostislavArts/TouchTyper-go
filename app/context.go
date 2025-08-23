e9af12a6ebf920605fa9a04e7142931ed7f97842
RostislavArts <155669656+RostislavArts@users.noreply.github.com>
2025-08-23 03:06:40 +0300

    Updated default word list

--- app/context.go
+++ app/context.go
@@ -143,8 +143,26 @@ func (c *Context) Load() {
 
 func (c *Context) loadWordLists(basePath string) {
 	// Default word list if no files found
-	defaultWords := []string{"the", "be", "to", "of", "and", "a", "in", "that",
-		"have", "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at"}
+	defaultWords := []string{"the", "be", "of", "and", "a", "to", "in", "he", "have",
+		"it", "that", "for", "they", "I", "with", "as", "not", "on", "she", "at", "by",
+		"this", "we", "you", "do", "but", "from", "or", "which", "one", "would", "all",
+		"will", "there", "say", "who", "make", "when", "can", "more", "if", "no", "man",
+		"out", "other", "so", "what", "time", "up", "go", "about", "than", "into", "could",
+		"state", "only", "new", "year", "some", "take", "come", "these", "know", "see", "use",
+		"get", "like", "then", "first", "any", "work", "now", "may", "such", "give", "over",
+		"think", "most", "even", "find", "day", "also", "after", "way", "many", "must", "look",
+		"before", "great", "back", "through", "long", "where", "much", "should", "well", "people",
+		"down", "own", "just", "because", "good", "each", "those", "feel", "seem", "how", "high",
+		"too", "place", "little", "world", "very", "still", "nation", "hand", "old", "life", "tell",
+		"write", "become", "here", "show", "house", "both", "between", "need", "mean", "call",
+		"develop", "under", "last", "right", "move", "thing", "general", "school", "never",
+		"same", "another", "begin", "while", "number", "part", "turn", "real", "leave", "might",
+		"want", "point", "form", "off", "child", "few", "small", "since", "against", "ask",
+		"late", "home", "interest", "large", "person", "end", "open", "public", "follow",
+		"during", "present", "without", "again", "hold", "govern", "around", "possible", "head",
+		"consider", "word", "program", "problem", "however", "lead", "system", "set", "order", "eye",
+		"plan", "run", "keep", "face", "fact", "group", "play", "stand", "increase", "early", "course",
+		"change", "help", "line"}
 	c.WordLists = []config.WordList{{Name: "Default", Words: defaultWords}}
 
 	if _, err := os.Stat(basePath); os.IsNotExist(err) {
